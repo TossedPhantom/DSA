2e03baf032425bfb3f5046c17f838bbb05c67b92
TossedPhantom <tossedphantom@gmail.com>
2026-08-06 20:25:45 +0530

    Fixed logic bugs in recursive binary search.

--- bsearch_recurs.c
+++ bsearch_recurs.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int bsearch(int arr[], int start, int end, int target){
+    if(start > end) return -1;
     int middle = (start+end)/2;
-    if(target == middle) return middle;
-    else if(target < middle) return bsearch(arr, start, middle, target);
-    else return bsearch(arr, middle, end, target);
+    if(target == arr[middle]) return middle;
+    else if(target < arr[middle]) return bsearch(arr, start, middle-1, target);
+    else return bsearch(arr, middle+1, end, target);
 }
 
 int main(){
@@ -13,14 +14,11 @@ int main(){
     printf("Enter number to search: ");
     scanf("%d", &target);
     int start = 0;
-    int end = N;
-    if(target < start || target > end){
-        printf("%d not found in array containing 0-19.\n", target);
-        return 0;
-    }
+    int end = N-1;
 
     int result = bsearch(arr, start, end, target);
-    printf("%d found at %d.\n", target, result);
+    if(result == -1) printf("%d not found in array containing %d-%d.\n", target, arr[start], arr[end]);
+    else printf("%d found at %d.\n", target, result);
 
     return 0;
 }
